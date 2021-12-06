95f47183c4e72aff9b3e134b73da611e9d1c950d
Allaye <kolade199@gmail.com>
2021-12-06 23:31:08 +0100

    initialize the lenet layers

--- ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
@@ -54,7 +54,13 @@ class LeNet(nn.Module):
     
     '''
     def __init__(self):
-        pass
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv3 = nn.Conv2d(16, 120, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
 
     def forward(self, x):
         pass
