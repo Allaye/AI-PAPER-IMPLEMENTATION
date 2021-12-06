8be921b3e8ff0bb0868fa7006f5fb7d07c1039cb
Allaye <kolade199@gmail.com>
2021-12-06 22:55:39 +0100

    create preparation of the dataset loader

--- ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 
 
 def configure_device():
@@ -22,7 +24,32 @@ def hyperparameter():
     return lr, epochs, batch_size
 
 
+def prepare_dataset(batch_size):
+    '''
+    get the CIFAR10 dataset, transform it to tensor and normalize it
+    '''
+    # MNIST dataset
+    train_dataset = torchvision.datasets.CIFAR10(root='./',train=True,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
+    test_dataset = torchvision.datasets.CIFAR10(root='./', train=False, transform=transforms.ToTensor())
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return train_loader, test_loader, classes
+
+
+
 class LeNet(nn.Module):
+    '''
+    
+    '''
     def __init__(self):
         pass
 
