6e7284bbc79498a11320a193ff40516f9dec2297
Allaye <kolade199@gmail.com>
2021-12-07 21:48:26 +0100

    create the forward method

--- ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/lenet.py
@@ -44,7 +44,8 @@ def prepare_dataset(batch_size):
 
     return train_loader, test_loader, classes
 
-
+a, b, c = prepare_dataset(20)
+print(a)
 
 class LeNet(nn.Module):
     '''
@@ -55,15 +56,20 @@ class LeNet(nn.Module):
     '''
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.conv3 = nn.Conv2d(16, 120, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 6, 5, 1)
+        self.conv2 = nn.Conv2d(6, 16, 5, 1)
+        self.conv3 = nn.Conv2d(16, 120, 5, 1)
+        self.pool = nn.AvgPool2d(2, 2)
         self.fc1 = nn.Linear(120, 84)
         self.fc2 = nn.Linear(84, 10)
 
     def forward(self, x):
-        pass
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 120)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
 
     def loss_optimizer(self):
         pass
