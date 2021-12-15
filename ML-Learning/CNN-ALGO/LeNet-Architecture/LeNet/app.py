0c8e1614e36565e1604fb06f9ca504e5f52efabb
Allaye <kolade199@gmail.com>
2021-12-15 22:48:59 +0100

    flask app

---
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+# from LeNet.src import inferance
+from src import inferance
+app = Flask(__name__)
+
+
+@app.get('/')
+def home():
+    d = inferance
+    print(d)
+    return jsonify(f'Hello World!, this is an endpoint for introducing the LeNet model in PyTorch.')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
