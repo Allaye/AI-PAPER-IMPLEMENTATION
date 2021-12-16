1283d29687f52ec8ecc559bbf49d12e405eaa001
Allaye <kolade199@gmail.com>
2021-12-17 00:18:48 +0100

    added new config file to hold config keys

---
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/src/configuration.py
@@ -0,0 +1,20 @@
+import torch
+
+
+def configure_device():
+    '''
+    check if GPU is available and use it
+    '''
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("device:", device)
+    return device
+
+
+def hyperparameter():
+    '''
+    define hyper parameters
+    '''
+    lr = 0.001
+    epochs = 10
+    batch_size = 100
+    return lr, epochs, batch_size
