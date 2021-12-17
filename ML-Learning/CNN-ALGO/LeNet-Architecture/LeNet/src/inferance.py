490af7cef3ef1139fe97553cfa2acf7d94dc279e
Allaye <kolade199@gmail.com>
2021-12-17 01:46:51 +0100

    added function to load and process test data

--- ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/src/inferance.py
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/src/inferance.py
@@ -1,4 +1,5 @@
 import torch
+from torchvision import transforms, datasets
 from lenet import LeNet
 
 
@@ -8,3 +9,9 @@ def load_checkpoint(filepath):
     model = LeNet()
     model.load_state_dict(checkpoint["model_state_dict"])
     return model
+
+def prepare_test_set(imagepath):
+    dataset = datasets.ImageFolder(root=imagepath, transform=transforms.Compose([transforms.ToTensor(), transforms.Grayscale(), transforms.Resize((32, 32))]))
+    return dataset
+def make_inference(model, test_loader):
+    pass
