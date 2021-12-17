4c9918c3f6df0adf8b76075c083502d329ad161f
Allaye <kolade199@gmail.com>
2021-12-17 18:09:00 +0100

    update prepare date function

--- ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/src/inferance.py
+++ ML-Learning/CNN-ALGO/LeNet-Architecture/LeNet/src/inferance.py
@@ -13,19 +13,7 @@ def load_checkpoint(filepath):
     model.load_state_dict(checkpoint["model_state_dict"])
     return model
 
-def prepare_test_set(imagepath, exten=(".jpg", ".png", ".jpeg")):
-    # dataset = datasets.ImageFolder(root=imagepath, transform=transforms.Compose([transforms.ToTensor()]))
-    filenames = []
-    for file in os.scandir(imagepath):
-        if (file.is_file() and file.name.endswith(exten)):
-            ## print(file.path)
-            filenames.append(file.path)
-    batch_size = len(filenames)
-    batches = torch.zeros(batch_size, 3, 32, 32, dtype=torch.uint8)
-    for i, filename in enumerate(filenames):
-        batches[i] = transforms.transforms.Resize((32, 32))(torchvision.io.read_image(filename))
-    
-    return batches
+
 
 
 def make_inference(model, test_loader):
