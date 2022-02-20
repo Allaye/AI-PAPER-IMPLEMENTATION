272fdc01e1f0149af5417d72fbcc9db4de7d29ae
Allaye <kolade199@gmail.com>
2022-02-20 20:11:20 +0100

    add new feature

--- ML-Learning/LeNet-Architecture/LeNet/src/inference.py
+++ ML-Learning/LeNet-Architecture/LeNet/src/inference.py
@@ -1,8 +1,6 @@
 import torch
-import numpy as np
-from PIL import Image
-from lenet import LeNet
 from dataset_loader import prepare_testset
+from lenet import LeNet
 
 
 def load_checkpoint(filepath):
@@ -14,36 +12,12 @@ def load_checkpoint(filepath):
     return model
 
 
-m = load_checkpoint("models/checkpoint18.pth")
-# print(m["model_state_dict"])
-# print(m["best_accuracy"])
-# print(m["epoch"])
-
-data = prepare_testset("img")
-torch.no_grad()
-im = Image.open("img/cat.jpeg").convert("RGB")
-# im.show()
-print(data.shape)
-print(data[0::].shape)
-outputs = m(data)
-print(outputs.shape)
-_, predicted = torch.max(outputs.data, 1)
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-# print(classes[predicted.item()])
-print(predicted)
-
-
-# print(predicted.data)
-# print(outputs.data == outputs)
-# for j in range(data.__len__()):
-# print("Predicted: ", classes[predicted[j]])
-# print(predicted, classes[ predicted[j]])
-# pred = m(data[1::])
-# predicted_class = np.argmax(pred.detach().numpy())
-
-
-def make_inference(model, test_set):
-    data = prepare_testset(test_set)
-    model = load_checkpoint("./models/checkpoint18.pth")
-
+def make_inference(modelpath, test_set):
+    batch_data, classes = prepare_testset(test_set)
+    inference_model = load_checkpoint(modelpath)
+    outputs = inference_model(batch_data)
+    _, prediction = torch.max(outputs.data, 1)
+    for i in range(batch_data.__len__()):
+        print("Predicted: ", classes[prediction[i]])
+        print(prediction, classes[prediction[i]])
+    return prediction
