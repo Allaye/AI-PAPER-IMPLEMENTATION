e44a58f8ff0be65af79776f9460c0249b0e80bbf
Allaye <kolade199@gmail.com>
2022-02-20 11:04:27 +0100

    added new flask routes

---
+++ ML-Learning/LeNet-Architecture/LeNet/src/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from . import inferance
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def predict():
+    d = inferance
+    print(d)
+    return jsonify(f'Hello World!, this is an endpoint for introducing the LeNet model in PyTorch.')
