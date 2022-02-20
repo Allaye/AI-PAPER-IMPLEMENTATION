6b4bf13c604be816940787bd8e5f8ed201f513a9
Allaye <kolade199@gmail.com>
2022-02-20 11:12:11 +0100

    cleaned dataset_loader

--- ML-Learning/LeNet-Architecture/LeNet/src/dataset_loader.py
+++ ML-Learning/LeNet-Architecture/LeNet/src/dataset_loader.py
@@ -5,17 +5,19 @@ import torchvision.transforms as transforms
 
 
 def prepare_dataset(batch_size):
-    '''
+    """
     get the CIFAR10 dataset, transform it to tensor, convert it into grayscale and normalize it
     return a trainloader, testloader and data classes
-    '''
+    """
     # CIFAR10 dataset
 
-    train_dataset = torchvision.datasets.CIFAR10(root='./',train=True,
-                                               transform=transforms.Compose([transforms.ToTensor(), transforms.Grayscale()]),
-                                               download=True)
-    test_dataset = torchvision.datasets.CIFAR10(root='./', train=False, transform=transforms.Compose([transforms.ToTensor(), transforms.Grayscale()]))
-    
+    train_dataset = torchvision.datasets.CIFAR10(root='./', train=True,
+                                                 transform=transforms.Compose(
+                                                     [transforms.ToTensor(), transforms.Grayscale()]),
+                                                 download=True)
+    test_dataset = torchvision.datasets.CIFAR10(root='./', train=False, transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Grayscale()]))
+
     # Data loader
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
@@ -27,20 +29,21 @@ def prepare_dataset(batch_size):
     return train_loader, test_loader, classes
 
 
-def prepare_testset(imagepath, exten=(".jpg", ".png", ".jpeg")):
+def prepare_testset(imagepath, ext=(".jpg", ".png", ".jpeg")):
     """
     prepare dataset to use for normal testing, this function accepts a path to the file and the extension of the file
     and return a tensor of the images or image
     
     """
-    torchvision
+    # get the image
     filenames = []
     for file in os.scandir(imagepath):
 
-        if (file.is_file() and file.name.endswith(exten)):
+        if file.is_file() and file.name.endswith(ext):
             filenames.append(file.path)
     batch_size = len(filenames)
     batches = torch.zeros(batch_size, 1, 32, 32, dtype=torch.float)
     for i, filename in enumerate(filenames):
-        batches[i] = transforms.Compose([transforms.Resize((32, 32)), transforms.Grayscale()])(torchvision.io.read_image(filename))
+        batches[i] = transforms.Compose([transforms.Resize((32, 32)), transforms.Grayscale()])(
+            torchvision.io.read_image(filename))
     return batches
