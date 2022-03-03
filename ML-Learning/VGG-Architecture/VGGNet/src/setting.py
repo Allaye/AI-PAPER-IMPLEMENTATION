92cbf0a1007e941599e537f4801ff2e51dda5c45
Allaye <kolade199@gmail.com>
2022-03-03 11:58:31 +0100

    updates

--- ML-Learning/VGG-Architecture/VGGNet/src/setting.py
+++ ML-Learning/VGG-Architecture/VGGNet/src/setting.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def config_device():
+def config_device() -> torch.device:
     """
     Set device for training and testing, choose GPU if available else default to CPU
     """
@@ -9,3 +9,13 @@ def config_device():
     print(f'{device}device selected')
     return device
 
+
+def hyperparameter() -> tuple:
+    """
+    static Hyperparameters setting for training
+    """
+    # hyperparameters
+    epochs = 50
+    lr = 0.001
+    batch_size = 64
+    return epochs, lr, batch_size
