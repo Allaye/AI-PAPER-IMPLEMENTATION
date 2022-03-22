7e677e4910da756d7da6c76152411648fad3a6d7
Allaye <kolade199@gmail.com>
2022-03-22 14:30:43 +0100

    working on inception net

---
+++ ML-Learning/GoogleNet-Architecture/GoogleNet/InceptionNet.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.prelu(x)
+
+
+class InceptionNet(nn.Module):
+    pass
