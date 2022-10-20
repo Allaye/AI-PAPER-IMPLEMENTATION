ab30e28481c5e6e0de0c21e926e1da4b92096274
Kolade Gideon <kolade199@gmail.com>
2022-10-20 11:42:31 +0000

    constructing googlenet architecture

--- GoogleNet-Architecture/GoogleNet/src/InceptionNet.py
+++ GoogleNet-Architecture/GoogleNet/src/InceptionNet.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class ConvBlock(nn.Module):
+    """
+    a factory class to create basic convolution network
+    """
     def __init__(self, in_channels, out_channels, **kwargs):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
@@ -14,26 +16,29 @@ class ConvBlock(nn.Module):
         return self.relu(self.bn(self.conv(x)))
 
 
-class InceptionNet(nn.Module):
+class InceptionBlock(nn.Module):
+    """
+    a conv class that uses the convblock to create the inceptionNet block
+    """
     def __init__(self, in_channels, in_1x1, in_3x3reduce, in_3x3, in_5x5reduce, in_5x5, in_1x1pool):
-        super(InceptionNet, self).__init__()
-        self.incep_1 = ConvBlock(in_channels, in_1x1, kernel_size=1, padding='same')
-        self.incep_3 = nn.Sequential(
+        super(InceptionBlock, self).__init__()
+        self.incepBlock1 = ConvBlock(in_channels, in_1x1, kernel_size=1, padding='same')
+        self.incepBlock3 = nn.Sequential(
             ConvBlock(in_channels, in_3x3reduce, kernel_size=1, padding='same'),
             ConvBlock(in_3x3reduce, in_3x3, kernel_size=3, padding='same'))
-        self.incep_5 = nn.Sequential(
+        self.incepBlock5 = nn.Sequential(
             ConvBlock(in_channels, in_5x5reduce, kernel_size=1, padding='same'),
             ConvBlock(in_5x5reduce, in_5x5, kernel_size=5, padding='same'))
-        self.incep_1pool = nn.Sequential(
+        self.incepBlockpool1 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding='same'),
             ConvBlock(in_channels, in_1x1pool, kernel_size=1, padding='same'))
 
     def forward(self, x):
         return torch.cat([
-            self.incep_1(x),
-            self.incep_3(x),
-            self.incep_5(x),
-            self.incep_1pool(x)
+            self.incepBlock1(x),
+            self.incepBlock3(x),
+            self.incepBlock5(x),
+            self.incepBlockpool1(x)
         ], 1)
 
 
