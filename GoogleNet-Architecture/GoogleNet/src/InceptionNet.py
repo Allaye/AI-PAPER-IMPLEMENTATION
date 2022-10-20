0d5818a8a8fbcda4e9d3faa61de2698865fbf206
Kolade Gideon <kolade199@gmail.com>
2022-10-20 16:00:43 +0000

    fix error causing 4D/3D to 2D error

--- GoogleNet-Architecture/GoogleNet/src/InceptionNet.py
+++ GoogleNet-Architecture/GoogleNet/src/InceptionNet.py
@@ -20,18 +20,18 @@ class InceptionBlock(nn.Module):
     """
     a conv class that uses the convblock to create the inceptionNet block
     """
-    def __init__(self, in_channels, in_1x1, in_3x3reduce, in_3x3, in_5x5reduce, in_5x5, in_1x1pool):
+    def __init__(self, in_channels, out_1x1, out_3x3reduce, out_3x3, out_5x5reduce, out_5x5, out_1x1pool):
         super(InceptionBlock, self).__init__()
-        self.incepBlock1 = ConvBlock(in_channels, in_1x1, kernel_size=1, padding='same')
+        self.incepBlock1 = ConvBlock(in_channels, out_1x1, kernel_size=1, padding='same')
         self.incepBlock3 = nn.Sequential(
-            ConvBlock(in_channels, in_3x3reduce, kernel_size=1, padding='same'),
-            ConvBlock(in_3x3reduce, in_3x3, kernel_size=3, padding='same'))
+            ConvBlock(in_channels, out_3x3reduce, kernel_size=1, padding='same'),
+            ConvBlock(out_3x3reduce, out_3x3, kernel_size=3, padding='same'))
         self.incepBlock5 = nn.Sequential(
-            ConvBlock(in_channels, in_5x5reduce, kernel_size=1, padding='same'),
-            ConvBlock(in_5x5reduce, in_5x5, kernel_size=5, padding='same'))
+            ConvBlock(in_channels, out_5x5reduce, kernel_size=1, padding='same'),
+            ConvBlock(out_5x5reduce, out_5x5, kernel_size=5, padding=2))
         self.incepBlockpool1 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=3, stride=1, padding='same'),
-            ConvBlock(in_channels, in_1x1pool, kernel_size=1, padding='same'))
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            ConvBlock(in_channels, out_1x1pool, kernel_size=1, padding='same'))
 
     def forward(self, x):
         return torch.cat([
